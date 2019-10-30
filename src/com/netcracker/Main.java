c4b70dce42c8c52f85d7db70c6cae4484eee77d2
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 20:00:07 +0300

    BubbleSort time estimation sample in main

--- src/com/netcracker/Main.java
+++ src/com/netcracker/Main.java
@@ -1,8 +1,17 @@
 package com.netcracker;
 
+import java.util.Arrays;
+
 public class Main {
+    final static int N = 10;
 
     public static void main(String[] args) {
+        TimeEstimator timeEstimator = new TimeEstimator();
 
+        int[] array = new int[N];
+        ArraySorter.getRandomArray(array, 100);
+        ArraySorter.printArray(array);
+        timeEstimator.estimateSortingTime(ArraySorter::BubbleSort, array, SortType.ASCENDING, "BubbleSort(array, SortType.ASCENDING)");
+        ArraySorter.printArray(array);
     }
 }
