120ea3967e76be40a1ca4870e5867d27ccb24b40
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 23:46:10 +0300

    drawRectangle added

--- src/com/netcracker/SharpDrawer.java
+++ src/com/netcracker/SharpDrawer.java
@@ -1,4 +1,12 @@
 package com.netcracker;
 
 public class SharpDrawer {
+    public static void drawRectangle(int row, int col) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+    }
 }
