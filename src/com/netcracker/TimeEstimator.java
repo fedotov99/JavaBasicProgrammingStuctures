21c8c66da350d083b6d1f1b2498b53571f6eb533
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 23:43:50 +0300

    methods of TimeEstimator became static

--- src/com/netcracker/TimeEstimator.java
+++ src/com/netcracker/TimeEstimator.java
@@ -11,14 +11,14 @@ public class TimeEstimator {
         public void getFactorial(int n);
     }
 
-    public void estimateSortingTime(PassedSortingFunction function, int[] array, SortType sortType, String functionName) {
+    public static void estimateSortingTime(PassedSortingFunction function, int[] array, SortType sortType, String functionName) {
         long startTime = System.nanoTime();
         function.sortArray(array, sortType);
         long estimatedTime = System.nanoTime() - startTime;
         System.out.println("The estimated time of " + functionName + " is " + estimatedTime + "ms");
     }
 
-    public void estimateFactorialTime(PassedFactorialFunction function, int n, String functionName) {
+    public static void estimateFactorialTime(PassedFactorialFunction function, int n, String functionName) {
         long startTime = System.nanoTime();
         function.getFactorial(n);
         long estimatedTime = System.nanoTime() - startTime;
