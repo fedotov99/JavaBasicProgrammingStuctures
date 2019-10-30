0573655f33b30c965c31080f515d5017480ebc83
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 23:17:34 +0300

    all sorting methods' samples added

--- src/com/netcracker/Main.java
+++ src/com/netcracker/Main.java
@@ -3,20 +3,22 @@ package com.netcracker;
 import java.util.Arrays;
 
 public class Main {
-    final static int N = 5;
+    final static int N = 100;
 
     public static void main(String[] args) {
         TimeEstimator timeEstimator = new TimeEstimator();
 
         int[] array = new int[N];
         ArraySorter.getRandomArray(array, 100);
-//        ArraySorter.printArray(array);
-        timeEstimator.estimateSortingTime(ArraySorter::BubbleSort, array, SortType.ASCENDING, "BubbleSort(array, SortType.ASCENDING)");
+        timeEstimator.estimateSortingTime(ArraySorter::bubbleSort, array, SortType.ASCENDING, "bubbleSort(array, SortType.ASCENDING)");
 //        ArraySorter.printArray(array);
 
         ArraySorter.getRandomArray(array, 100);
+        timeEstimator.estimateSortingTime(ArraySorter::selectionSort, array, SortType.ASCENDING, "selectionSort(array, SortType.ASCENDING)");
 //        ArraySorter.printArray(array);
-        timeEstimator.estimateSortingTime(ArraySorter::SelectionSort, array, SortType.ASCENDING, "SelectionSort(array, SortType.ASCENDING)");
+
+        ArraySorter.getRandomArray(array, 100);
+        timeEstimator.estimateSortingTime(ArraySorter::callStandardSortMethod, array, SortType.ASCENDING, "callStandardSortMethod(array, SortType.ASCENDING)");
 //        ArraySorter.printArray(array);
     }
 }
