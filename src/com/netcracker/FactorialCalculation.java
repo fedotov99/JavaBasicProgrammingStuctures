2c568a9ce8be94636149f62ef3470bebbc4d2b52
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 23:27:28 +0300

    getLoopFactorial added

--- src/com/netcracker/FactorialCalculation.java
+++ src/com/netcracker/FactorialCalculation.java
@@ -1,4 +1,15 @@
 package com.netcracker;
 
 public class FactorialCalculation {
+    public static int getLoopFactorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+//    public static int getRecursionFactorial(int n) {
+//
+//    }
 }
