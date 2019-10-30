b7b8fada3305f496fb55cf8d1019ff6f19458d1e
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 22:47:48 +0300

    SelectionSort estimating time sample added

--- src/com/netcracker/Main.java
+++ src/com/netcracker/Main.java
@@ -3,15 +3,20 @@ package com.netcracker;
 import java.util.Arrays;
 
 public class Main {
-    final static int N = 10;
+    final static int N = 5;
 
     public static void main(String[] args) {
         TimeEstimator timeEstimator = new TimeEstimator();
 
         int[] array = new int[N];
         ArraySorter.getRandomArray(array, 100);
-        ArraySorter.printArray(array);
+//        ArraySorter.printArray(array);
         timeEstimator.estimateSortingTime(ArraySorter::BubbleSort, array, SortType.ASCENDING, "BubbleSort(array, SortType.ASCENDING)");
-        ArraySorter.printArray(array);
+//        ArraySorter.printArray(array);
+
+        ArraySorter.getRandomArray(array, 100);
+//        ArraySorter.printArray(array);
+        timeEstimator.estimateSortingTime(ArraySorter::SelectionSort, array, SortType.ASCENDING, "SelectionSort(array, SortType.ASCENDING)");
+//        ArraySorter.printArray(array);
     }
 }
