c2a3a10e4db6033d3f446fce6137f84a311d4f71
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-31 00:03:06 +0300

    drawEmptyRectangle added

--- src/com/netcracker/SharpDrawer.java
+++ src/com/netcracker/SharpDrawer.java
@@ -11,6 +11,21 @@ public class SharpDrawer {
         System.out.println();
     }
 
+    public static void drawEmptyRectangle(int row, int col) {
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= col; j++) {
+                if (i == 1 || i == row || j == 1 || j == col) {  // for the first and last row print all #. For the first and last column also print all #
+                    System.out.print("#");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
     public static void drawTriangle(int size) {
         for (int i = 1; i <= size; i++) {
             for (int j = 1; j <= i; j++) {
