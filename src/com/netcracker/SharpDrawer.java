4e0e6ed4e442d9c7f5683c73752e2469150013d9
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 23:56:56 +0300

    drawTriangle added

--- src/com/netcracker/SharpDrawer.java
+++ src/com/netcracker/SharpDrawer.java
@@ -8,5 +8,16 @@ public class SharpDrawer {
             }
             System.out.println();
         }
+        System.out.println();
+    }
+
+    public static void drawTriangle(int size) {
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("#");
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
