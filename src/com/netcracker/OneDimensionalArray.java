a9902c62cb11d6a14e797a1dc721b486e335a585
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-31 00:33:05 +0300

    getOddNumberArray added

--- src/com/netcracker/OneDimensionalArray.java
+++ src/com/netcracker/OneDimensionalArray.java
@@ -1,4 +1,20 @@
 package com.netcracker;
 
 public class OneDimensionalArray {
+    public static int[] getOddNumberArray(SortType sortType) {
+        int[] array = new int[50];
+        int oddNumber = 1;
+        for (int i = 0; i < array.length; i++) {
+            array[i] = oddNumber;
+            oddNumber += 2;
+        }
+
+        if (sortType == SortType.ASCENDING) {
+            return  array;
+        }
+        else {
+            ArraySorter.selectionSort(array, SortType.DESCENDING);
+            return array;
+        }
+    }
 }
