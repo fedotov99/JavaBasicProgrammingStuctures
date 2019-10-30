e1b75d4e8831c36c5af847ea15a5fc7595475170
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 23:44:14 +0300

    methods of TimeEstimator became static

--- src/com/netcracker/Main.java
+++ src/com/netcracker/Main.java
@@ -6,26 +6,25 @@ public class Main {
     final static int N = 100;
 
     public static void main(String[] args) {
-        TimeEstimator timeEstimator = new TimeEstimator();
 //        ===============ArraySorter sample===============
 
         int[] array = new int[N];
         ArraySorter.getRandomArray(array, 100);
-        timeEstimator.estimateSortingTime(ArraySorter::bubbleSort, array, SortType.ASCENDING, "bubbleSort(array, SortType.ASCENDING)");
+        TimeEstimator.estimateSortingTime(ArraySorter::bubbleSort, array, SortType.ASCENDING, "bubbleSort(array, SortType.ASCENDING)");
 //        ArraySorter.printArray(array);
 
         ArraySorter.getRandomArray(array, 100);
-        timeEstimator.estimateSortingTime(ArraySorter::selectionSort, array, SortType.ASCENDING, "selectionSort(array, SortType.ASCENDING)");
+        TimeEstimator.estimateSortingTime(ArraySorter::selectionSort, array, SortType.ASCENDING, "selectionSort(array, SortType.ASCENDING)");
 //        ArraySorter.printArray(array);
 
         ArraySorter.getRandomArray(array, 100);
-        timeEstimator.estimateSortingTime(ArraySorter::callStandardSortMethod, array, SortType.ASCENDING, "callStandardSortMethod(array, SortType.ASCENDING)");
+        TimeEstimator.estimateSortingTime(ArraySorter::callStandardSortMethod, array, SortType.ASCENDING, "callStandardSortMethod(array, SortType.ASCENDING)");
 //        ArraySorter.printArray(array);
 
 //        ===============FactorialCalculation sample===============
         int n = 5;
-        timeEstimator.estimateFactorialTime(FactorialCalculation::getLoopFactorial, n, "getLoopFactorial(n)");
-        timeEstimator.estimateFactorialTime(FactorialCalculation::getRecursionFactorial, n, "getRecursionFactorial(n)");
+        TimeEstimator.estimateFactorialTime(FactorialCalculation::getLoopFactorial, n, "getLoopFactorial(n)");
+        TimeEstimator.estimateFactorialTime(FactorialCalculation::getRecursionFactorial, n, "getRecursionFactorial(n)");
 
     }
 }
