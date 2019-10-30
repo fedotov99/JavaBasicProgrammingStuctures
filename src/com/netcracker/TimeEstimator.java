110e17355f44f3d276754b2cdaa0e22c502f4767
Vlad Fedotov <fedotovvlad@list.ru>
2019-10-30 15:51:49 +0300

    TimeEstimator class is written

--- src/com/netcracker/TimeEstimator.java
+++ src/com/netcracker/TimeEstimator.java
@@ -1,4 +1,14 @@
 package com.netcracker;
 
 public class TimeEstimator {
+    public interface PassedFunction {
+        public void doSomething();
+    }
+
+    public String estimateTime(PassedFunction function, String functionName) {
+        long startTime = System.nanoTime();
+        function.doSomething();
+        long estimatedTime = System.nanoTime() - startTime;
+        return "The estimated time of " + functionName + " is " + estimatedTime;
+    }
 }
