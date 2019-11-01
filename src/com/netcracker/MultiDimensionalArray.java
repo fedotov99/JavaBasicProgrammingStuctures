cf40d9819d904231e166d113bfea9387c839cf1c
Vlad Fedotov <fedotovvlad@list.ru>
2019-11-01 15:30:55 +0300

    getRandomMultiArray and printing method added

--- src/com/netcracker/MultiDimensionalArray.java
+++ src/com/netcracker/MultiDimensionalArray.java
@@ -1,4 +1,19 @@
 package com.netcracker;
 
+import java.util.Arrays;
+
 public class MultiDimensionalArray {
+    public static int[][] getRandomMultiArray(int rows, int cols, int min, int max) {
+        int[][] array = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            array[i] = OneDimensionalArray.getRandomArray(cols, min, max);
+        }
+        return array;
+    }
+
+    public static void printMultiArray(int[][] array) {
+        System.out.println();
+        System.out.println(Arrays.deepToString(array));
+        System.out.println();
+    }
 }
